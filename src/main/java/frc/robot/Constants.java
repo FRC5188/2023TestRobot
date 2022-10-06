1f357d654ea46d26111c662482119e89d01e146a
first5188 <first5188@gmail.com>
2022-10-06 19:58:02 -0400

    Drivetrain code

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,4 +12,8 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    public static final double QUICK_TURN_DEADBAND = 0.05;
+    public static final double QUICK_TURN_MULTIPLIER = 0.75;
+    public static final double ARCADE_DRIVE_DEADBAND = 0.025;
+}
--- src/main/java/frc/robot/subsystems/driveTrain.java
+++ src/main/java/frc/robot/subsystems/driveTrain.java
@@ -6,50 +6,72 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
-
+import frc.robot.Constants;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class driveTrain extends SubsystemBase {
 
-  // Declaring our four motors for the wheels
-  WPI_TalonFX leftPrimary;
-  WPI_TalonFX leftSecondary;
-  WPI_TalonFX rightPrimary;
-  WPI_TalonFX rightSecondary;
+    // Declaring our four motors for the wheels
+    WPI_TalonFX leftPrimary;
+    WPI_TalonFX leftSecondary;
+    WPI_TalonFX rightPrimary;
+    WPI_TalonFX rightSecondary;
 
-  /** Creates a new driveTrain. */
-  public driveTrain() {
-    // Initializing our motors
-    leftPrimary = new WPI_TalonFX(0);
-    leftSecondary = new WPI_TalonFX(0);
-    rightPrimary = new WPI_TalonFX(0);
-    rightSecondary = new WPI_TalonFX(0);
+    /** Creates a new driveTrain. */
+    public driveTrain() {
+        // Initializing our motors
+        leftPrimary = new WPI_TalonFX(0);
+        leftSecondary = new WPI_TalonFX(0);
+        rightPrimary = new WPI_TalonFX(0);
+        rightSecondary = new WPI_TalonFX(0);
 
-    // Making the back motors follow the front motors
-    leftSecondary.follow(leftPrimary);
-    rightSecondary.follow(rightPrimary);
+        // Making the back motors follow the front motors
+        leftSecondary.follow(leftPrimary);
+        rightSecondary.follow(rightPrimary);
 
-    // Inverting the right motors so robot doesn't spin when setting motors to the same value
-    rightPrimary.setInverted(true);
-    rightSecondary.setInverted(true);
+        // Inverting the right motors so robot doesn't spin when setting motors to the
+        // same value
+        rightPrimary.setInverted(true);
+        rightSecondary.setInverted(true);
 
-    leftPrimary.setNeutralMode(NeutralMode.Brake);
-    leftSecondary.setNeutralMode(NeutralMode.Brake);
-    rightPrimary.setNeutralMode(NeutralMode.Brake);
-    rightSecondary.setNeutralMode(NeutralMode.Brake);
+        leftPrimary.setNeutralMode(NeutralMode.Brake);
+        leftSecondary.setNeutralMode(NeutralMode.Brake);
+        rightPrimary.setNeutralMode(NeutralMode.Brake);
+        rightSecondary.setNeutralMode(NeutralMode.Brake);
 
-  }
+    }
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
 
-  public void setleftpower(double power){
+    }
 
     
-  }
+    public void setMotorPower(double leftMotorPower, double rightMotorPower) {
+        // sets motor power , input of two doubles
+        rightPrimary.set(rightMotorPower);
+        leftPrimary.set(leftMotorPower);
+    }
+    //Takes throttle and rotation, converts into motor power, sets motors
+    public void arcadeDrive(double throttle, double rotation) {
+        double rDrive;
+        double lDrive;
+        //when going backwards, reverse rotation
+        if (throttle < -Constants.ARCADE_DRIVE_DEADBAND) {
+            rotation *= -1;
+        }
 
+        //
+        if (Math.abs(throttle) < Constants.QUICK_TURN_DEADBAND) {
+            lDrive = rotation * Constants.QUICK_TURN_MULTIPLIER;
+            rDrive = -rotation * Constants.QUICK_TURN_MULTIPLIER;
+        } else {
 
+            lDrive = throttle * (1 + Math.min(0, rotation));
+            rDrive = throttle * (1 - Math.max(0, rotation));
 
+        }
+        setMotorPower(lDrive, rDrive);
+    }
 }
